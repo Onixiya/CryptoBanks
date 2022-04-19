5a4b7cdd33ba1debe0cea669d7c42daa7161e521
silentstorm <mynutshang@cbt.com>
2022-04-20 05:46:56 +1000

    Add project files.

---
+++ ModMain.cs
@@ -0,0 +1,52 @@
+﻿using MelonLoader;
+using System.Collections.Generic;
+using HarmonyLib;
+using Assets.Scripts.Simulation.Towers.Behaviors;
+[assembly:MelonInfo(typeof(CryptoBanks.ModMain),"CryptoBanks","1.0.0","Silentstorm")]
+[assembly:MelonGame("Ninja Kiwi","BloonsTD6")]
+namespace CryptoBanks{
+    public class ModMain:MelonMod{
+        private static MelonLogger.Instance mllog=new MelonLogger.Instance("CryptoBanks");
+        public static void Log(object thingtolog,string type="msg"){
+            switch(type){
+                case"msg":
+                    mllog.Msg(thingtolog);
+                    break;
+                case"warn":
+                    mllog.Warning(thingtolog);
+                    break;
+                 case"error":
+                    mllog.Error(thingtolog);
+                    break;
+            }
+        }
+        public static string[]CSV=Data.BTCValues.Split('\n');
+        public static List<decimal>BTCValues=new List<decimal>();
+        public override void OnApplicationStart(){
+            foreach(string str in CSV){
+                string[]str1=str.Split(',');
+                if(decimal.TryParse(str1[2],out decimal value)){
+                    BTCValues.Add(value);
+                }
+            }
+        }
+        [HarmonyPatch(typeof(Bank),"PayInterest")]
+        public class BankPayInterest_patch{
+            [HarmonyPostfix]
+            public static void Postfix(ref Bank __instance){
+                if(new System.Random().Next(0,2)==1){
+                    __instance.cash*=(float)BTCValues[new System.Random().Next(2,BTCValues.Count+1)];
+                }else{
+                    __instance.cash/=(float)BTCValues[new System.Random().Next(2,BTCValues.Count+1)];
+                }
+            }
+        }
+        [HarmonyPatch(typeof(Bank),"Attatched")]
+        public class BankAttached_patch{
+            [HarmonyPostfix]
+            public static void Postfix(ref Bank __instance){
+                __instance.bankModel.capacity=float.MaxValue-1;
+            }
+        }
+    }
+}
